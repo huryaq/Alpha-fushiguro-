3ebd33d7c7806c12319fdda5db125ec9ffe262b6
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-08-19 14:20:39 +0530

    Update premium.py

--- plugins/premium.py
+++ plugins/premium.py
@@ -61,7 +61,7 @@ async def remove_premium_command(client: Client, message: Message):
         await message.reply(f"User {user_name} - {user_id} has been removed from premium users.")
         # Notify the user
         try:
-            await client.send_message(user_id, "Your Premium membersip has been ended contact admins to renew here - @DoraShin_hlo")
+            await client.send_message(user_id, "Your Premium membersip has been ended contact admins to renew here - @Amex_Fushiguro")
         except Exception as e:
             await message.reply(f"Failed to notify the user: {e}")
     else:
@@ -85,3 +85,4 @@ async def list_premium_command(client: Client, message: Message):
 
     user_list_text = "\n".join(user_list)
     await message.reply(f"Premium Users:\n{user_list_text}")
+
