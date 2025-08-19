600b5dfc05de31647fdf085c338b2cb020e1c117
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-08-19 08:29:28 +0530

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,82 @@
+from pyrogram import Client 
+from bot import Bot
+from config import OWNER_ID, ABOUT_TXT, HELP_TXT, START_MSG
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from database.database import add_user, del_user, full_userbase, present_user
+
+message_content = '''👋 <b>Hey {first}\n
+🎖️ <u>Available Plans</u>:</b>\n
+<blockquote expandable><i>● 30 rs For 7 Days Prime Membership\n
+● 110 rs For 1 Month Prime Membership\n
+● 299 rs For 3 Months Prime Membership\n
+● 550 rs For 6 Months Prime Membership\n
+● 999 rs For 1 Year Prime Membership</i></blockquote>\n
+💵 DM - <code>@Amex_Fushiguro</code>
+<b>(Tap to copy UPI Id)</b>\n\n
+♻️ <b>If payment is not getting sent on above given QR code then inform Admin, He will give you new QR code</b>\n
+‼️ ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ'''
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "help":
+        await query.message.edit_text(
+            text=HELP_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+                        InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')
+                    ]
+                ]
+            )
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+                     InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close')]
+                ]
+            )
+        )
+    elif data == "start":
+        await query.message.edit_text(
+            text=START_MSG.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
+                 InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')],
+                [InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')]
+            ])
+        )
+    elif data == "premium":
+        await query.message.reply_photo(
+            photo=("https://graph.org/file/ac61481c6c90015545d83-6b573a858fa21d40c6.jpg"),
+            caption=message_content.format(
+                first = query.from_user.mention, 
+                second = query.from_user.mention
+            ),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Owner", url="https://t.me/Amex_Fushiguro"),
+                        InlineKeyboardButton("Main Channel", url="https://t.me/Hanime_Web")
+                    ],
+                    [
+                        InlineKeyboardButton("🔒 Close", callback_data="close")
+                    ]
+                ]
+            )
+        )
+
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
