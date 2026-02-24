4bd801ae9ea7987a33e1daec65cb5fbcebf9c8bd
huryaq <hurryapurrya@gmail.com>
2026-02-24 18:29:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Aplha_ShortXBot")
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1001927339043"))
-FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "7566453683"))
+FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002398289912"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
